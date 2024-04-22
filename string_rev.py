1acd5c5790b361159048da2b790161691de1e18a
jay1467 global <krishnasurampudi7@gmail.com>
2024-04-22 21:12:29 +0530

    reverse new method added

--- string_rev.py
+++ string_rev.py
@@ -1,3 +1,9 @@
 x= "ayaj anhsirk" 
-reverese_words=[x[::-1] for x in x.split(" ")]
-print(reverese_words)
+nos="1234 5678" 
+# reverese_words=[x[::-1] for x in x.split(" ")]
+# print(reverese_words)
+reverse=""
+for char in nos[::-1]:
+    print(char)
+    reverse+=char
+print(reverse)
